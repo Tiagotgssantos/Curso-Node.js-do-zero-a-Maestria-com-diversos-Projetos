ecd26770cc03e480744408151b9317d987cef0bd
Tiago dos Santos <tiagotgssantostrade@gmail.com>
2023-08-26 12:48:03 -0300

    inicio de projeto conta

---
+++ 05_projeto1_accounts/index.js
@@ -0,0 +1,8 @@
+//modulos externos
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+//Core modules
+const fs = require('fs');
+
+console.log(chalk.bgGreen('Iniciamos o Accounts'));
