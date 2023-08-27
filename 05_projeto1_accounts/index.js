2f1085ef0068a66189af238854af0ae0719b96d9
Tiago dos Santos <tiagotgssantostrade@gmail.com>
2023-08-27 04:58:32 -0300

    7a a 74

--- 05_projeto1_accounts/index.js
+++ 05_projeto1_accounts/index.js
@@ -5,4 +5,96 @@ const chalk = require('chalk');
 //Core modules
 const fs = require('fs');
 
-console.log(chalk.bgGreen('Iniciamos o Accounts'));
+operation();
+
+function operation() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'O que você deseja fazer',
+                choices: [
+                    'Criar conta',
+                    'Consultar Saldo',
+                    'Depositar',
+                    'Sair'
+                ],
+            },
+        ])
+        .then((answer) =>{
+            const action = answer['action'];
+           if(action === 'Criar conta'){
+            createaAccount();
+           }
+        })
+        .catch((err) => console.log(err));
+}
+
+// criando conta 
+function createaAccount(){
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir'));
+
+    buildAccount();
+    /*chamando a função para 
+    digitar o nome da conta */
+
+}
+
+//criando a conta de fato:
+function buildAccount(){
+    inquirer
+    .prompt([
+        {
+            name: 'accountName',
+            message: 'Digite o nome para a sua conta:'
+        }
+    ])
+    .then((answer)=>{
+       const accountName = answer['accountName'];
+
+       console.info(accountName);
+
+       if(!fs.existsSync('accounts')){
+        //se o diretorio accounts não exitir.
+        fs.mkdirSync('accounts')
+        //crie o diretorio accounts
+       }
+       //criado o banco de dedados
+
+       if(fs.existsSync(`accounts/${accountName}.json`)){
+        /*vamos ver se o nome da conta existe, no diretorio accounts
+        que foi criado*/
+        //caso exista ele não deixa proceguir
+        //dessa forma tambem salvaremos em arquivo .json
+
+        console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'));
+        //mensagem de erro caso a conta ja exista.
+        /*caso ja exitas será direcionado a criar o
+        nome da conta com a função  
+        buildAccount(); assim voltando ao inicio
+        */
+        buildAccount();
+        return
+       }
+
+
+       /*Caso tenha passado pela validação 
+       vamos a criação da conta juntamento como arquivo json */
+       fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance":0}',
+        //conteudo do arquivo json seguindo os padroões do json
+        function(err){
+            console.log(err);
+        },
+       )
+
+       console.log(chalk.green('Parabéns, a sua conta foi criada!'));
+
+       operation();
+    })
+    .catch((err) => console.log(err));
+}
+
